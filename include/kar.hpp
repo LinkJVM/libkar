c5e7550f90e77a2e4b41a56afed69d0004bc5b43
kleiinnn <m@mklein.co.at>
2013-12-30 15:26:28 +0100

    let setPreference return void

--- include/kar.hpp
+++ include/kar.hpp
@@ -58,7 +58,7 @@ namespace kiss
 		bool addPreference(const QString& name, const QString& value);
 		bool hasPreference(const QString& name) const;
 		const QString& getPreference(const QString& name) const;
-		bool setPreference(const QString& name, const QString& value);
+		setPreference(const QString& name, const QString& value);
 		
 		const PreferenceMap& preferences() const;
 		void setPreferences(const PreferenceMap& preferences);
